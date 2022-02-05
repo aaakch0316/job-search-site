b206e970808553892ab9f4f7b5a49f4fd117c5fb
aaakch0316 <aaakch0316@gmail.com>
2022-02-05 15:31:20 +0900

    client [+] init

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      start pj
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h3>HOME</h3>
+    )
+}
+
+export default Home;
